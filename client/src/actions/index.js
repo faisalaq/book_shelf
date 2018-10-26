e5d0d01bd35ffbef600b712570cc0ee0ec6439e4
Faisal <faq01818@gmail.com>
2018-10-26 10:58:47 +0530

    S16 L168, Client: Home

---
+++ client/src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+export function getBooks(
+    limit = 10,
+    start = 0,
+    order = 'asc'
+){
+    // const request = `/api/books?limit=${limit}&skip=${start}&order=${order}`
+    const request = axios.get(`/api/books?limit=${limit}&skip=${start}&order=${order}`)
+                    .then((response => response.data))
+    console.log(request)
+
+    return {
+        type:'GET_BOOKS',
+        payload:request
+    }
+    
+}
--- client/src/components/Home/home.js
+++ client/src/components/Home/home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import HomeContainer from '../../containers/home_container'
+
+
 
 const Home = () => {
     return (
         <div>
-            HOME
+            <HomeContainer />
         </div>
     );
 };
