c753a46fbda87860c59289b98d596c5e4415c5cd
Faisal <faq01818@gmail.com>
2018-12-01 10:56:38 +0530

    S16 L184, Client:Logout Users

--- client/src/routes.js
+++ client/src/routes.js
@@ -10,6 +10,7 @@ import AddReview from './containers/Admin/add'
 import UserPosts from './components/Admin/userPosts'
 import EditReview from './containers/Admin/edit'
 import Register from './containers/Admin/register'
+import Logout from './components/Admin/logout'
 
 const Routes = () => {
     return (
@@ -17,6 +18,7 @@ const Routes = () => {
             <Switch>
                 <Route path='/' exact component={Auth(Home, null)}/>
                 <Route path='/login' exact component={Auth(Login, false)}/>
+                <Route path='/user/logout' exact component={Auth(Logout, true)}/>
                 <Route path='/user' exact component={Auth(User, true)}/>
                 <Route path='/user/add' exact component={Auth(AddReview,true)} />
                 <Route path='/user/register' exact component={Auth(Register,true)} />
