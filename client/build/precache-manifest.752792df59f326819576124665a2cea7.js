4a971db04a6b0dc5780e834dca533301d72b7924
Faisal <faq01818@gmail.com>
2018-12-01 16:36:32 +0530

    added build

---
+++ client/build/precache-manifest.752792df59f326819576124665a2cea7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a8b09bfa40044e7249a5",
+    "url": "/static/js/main.a8b09bfa.chunk.js"
+  },
+  {
+    "revision": "eb06dd3854f7677a1176",
+    "url": "/static/js/1.eb06dd38.chunk.js"
+  },
+  {
+    "revision": "6a59632b2e5836034b11ed3f03a9b645",
+    "url": "/index.html"
+  }
+];
